2e418bb1a5f93233ec784ed7a34599b05e125dba
LisaOz <frolovaie9@gmail.com>
2026-08-13 16:34:08 +0000

    Add checks to the script to verify that the file is provided, and that the provided file exists.

--- log_check.sh
+++ log_check.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
 
 read -p "Enter the log file: " LOGFILE
+
+if [ -z "$LOGFILE" ]; then
+    echo "ERROR: No log file provided."
+    exit 1
+fi
+
+if [ ! -f "$LOGFILE" ]; then
+    echo "ERROR: File  does not exist."
+    exit 1
+fi
+
 read -p "Username: " USERNAME  
 read -p "Enter hostname: " HOST
 echo 
@@ -8,7 +19,10 @@ echo "Analysing: $LOGFILE"
 
 echo "User: $USERNAME"
 echo "Log: $LOGFILE"
-echo "$HOST" 
+echo "$HOST"
+grep -ic failed "$LOGFILE"
+exit 0
+ 
 
 
 grep -ic failed "$LOGFILE"
