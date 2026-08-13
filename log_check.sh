d5b15e2480376e7aa8d434607779c6688f3d4331
LisaOz <frolovaie9@gmail.com>
2026-08-13 12:23:19 +0000

    Add hostname

--- log_check.sh
+++ log_check.sh
@@ -2,10 +2,13 @@
 
 read -p "Enter the log file: " LOGFILE
 read -p "Username: " USERNAME  
+read -p "Enter hostname: " HOST
 echo 
 echo "Analysing: $LOGFILE"
 
 echo "User: $USERNAME"
 echo "Log: $LOGFILE"
+echo "$HOST" 
+
 
 grep -ic failed "$LOGFILE"
